8b195773807b78d268ca49fc189886108695d187
Michael Herman <hermanmu@gmail.com>
2013-09-18 08:11:21 -0600

    updates

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 1. Generate two random numbers and print their mean, variance, and standard deviation.
 2. Calculate the mean, variance, and standard deviation for a list/array containing 10 random numbers.
-3. Generate three random numbers from 0 to 100 and print the largest number.
+3. Generate twenty random numbers from 0 to 100 and print the largest and smallest numbers.
---
+++ prob3/prob3.py
@@ -0,0 +1,23 @@
+from random import randrange
+
+def rand_n(n):
+    lst = []
+    for i in xrange(n):
+	lst.append(randrange(100))
+    return lst
+
+def max_n(lst):
+	return max(lst)
+
+def min_n(lst):
+	return min(lst)
+
+lst = rand_n(20)
+print "The numbers are " + str(lst)[1:-1]
+
+max_num = max_n(lst)
+print "The max number is " + str(max_num)
+
+min_num = min_n(lst)
+print "The min number is " + str(min_num)
+
