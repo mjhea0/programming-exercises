a2eaf587eb0107921a58671823fee8311c1882df
Michael Herman <hermanmu@gmail.com>
2013-09-23 08:19:21 -0600

    prob13

--- prob15/prob15.js
+++ prob15/prob15.js
@@ -1,4 +1,4 @@
-var createTriangle = function (num1,num2) {
+var createTriangleWhile = function (num1,num2) {
 	var line = "";
 	var i = 0;
 	while (i < num2) {
@@ -8,4 +8,14 @@ var createTriangle = function (num1,num2) {
 	}
 }
 
-createTriangle(2,10)
+var createTriangleFor = function (num1,num2) {
+	var line = "";
+	for (var i = 0; i < num2; i++) {
+		line += "#";
+		document.write(line + "<br/>")
+	}
+}
+
+createTriangleWhile(2,10)
+document.write("<br/>")
+createTriangleFor(2,10)
