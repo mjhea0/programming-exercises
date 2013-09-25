c61dec4ab1cef2f029b7b07c75a55f4fd57eea51
Michael Herman <hermanmu@gmail.com>
2013-09-24 23:46:28 -0600

    first jquery

---
+++ prob18/prob18.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("h2").click(function() {
+    alert("This is a sub header");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+});
