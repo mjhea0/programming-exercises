6be045f14630af8bb0be69c770536f2b01106d35
Michael Herman <michael@mherman.org>
2013-09-24 18:01:48 -0600

    updated crap code

--- prob01/prob1.js
+++ prob01/prob1.js
@@ -1,18 +1,24 @@
-var standard_deviation = function(x,y,average) {
+var variance = function(x,y,avg) {
 	var num1 = Math.pow((x - avg),2);
 	var num2 = Math.pow((y - avg),2);
-	var variance = (num1 + num2);
-	document.write("The variance is " + variance + "<br/>");
-	var std_dev = Math.sqrt((num1 + num2) / 2);
-	document.write("The standard dev is " + std_dev + ".");
+	return num1 + num2;
 }
+
+var standard_deviation = function(x) {
+	return Math.sqrt(x);
+}
+
 
 var num1 = Math.floor((Math.random() * (100)));
 var num2 = Math.floor((Math.random() * (100))); 
-
 document.write("The numbers are " + num1 + " and " + num2 + ".<br/>")
 
 var avg = (num1 + num2) / 2;
-document.write("The average is " + avg + ".<br/>");
+document.write("The average is " + avg + "<br/>");
 
-standard_deviation(num1, num2, avg)
+variance = variance(num1,num2,avg)
+document.write("The variance is " + variance + "<br/>");
+
+std_dev = standard_deviation(variance)
+document.write("The standard dev is " + std_dev);
+
