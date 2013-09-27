1755c2247b05a8d4f329fa577a75ca9dd6f126d5
Michael Herman <hermanmu@gmail.com>
2013-09-27 00:51:53 -0600

    background-colors

--- prob30/prob30.js
+++ prob30/prob30.js
@@ -1,17 +1,17 @@
 $(function() {
 
   $("button#red").click(function() {
-  	$("body").removeClass();
+    $("body").removeClass();
     $("body").addClass("red-background");
   })
 
   $("button#green").click(function() {
-  	$("body").removeClass();
+    $("body").removeClass();
     $("body").addClass("green-background");
   })
 
   $("button#blue").click(function() {
-  	$("body").removeClass();
+    $("body").removeClass();
     $("body").addClass("blue-background");
   })
 
