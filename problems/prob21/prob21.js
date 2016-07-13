4412ec1551a8248599d86d16c4078ab6e97ce889
Michael Herman <hermanmu@gmail.com>
2016-07-13 07:46:26 -0600

    Refactor

--- problems/prob21/prob21.js
+++ problems/prob21/prob21.js
@@ -1,6 +1,6 @@
-function reverse_string(str) {
-	return string.split("").reverse().join("")
+function reverse(str) {
+  return string.split('').reverse().join('');
 }
 
-var string = "Hi. I am a random string."
-document.write(reverse_string(string))
+var string = 'Hi. I am a random string.';
+console.log(reverse(string));
---
+++ problems/prob51/main.js
@@ -0,0 +1,25 @@
+//** definitions **//
+
+function isPalindrome(input) {
+  // sanitize string
+  var sanitizeString = sanitize(input);
+  return sanitizeString === reverseString(sanitizeString);
+}
+
+function sanitize(str) {
+  return str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'').replace(/\s/g, '');
+}
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+//** calls **//
+
+console.log(isPalindrome('rAceCar'));
+console.log(isPalindrome('anna'));
+console.log(isPalindrome('banana'));
+console.log(isPalindrome('I, man, am regal - a German am I'));
+console.log(isPalindrome('No lemons, no melon'));
+console.log(isPalindrome('Was it a car or a cat I saw?'));
+console.log(isPalindrome('Probably not a palindrome'));
