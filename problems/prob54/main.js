270c8c31ebb1985b92754a6fec45f2b4bf2e4675
Michael Herman <hermanmu@gmail.com>
2016-09-06 10:07:51 -0600

    added rotate matrix

--- problems/prob54/main.js
+++ problems/prob54/main.js
@@ -2,31 +2,43 @@
 
 Write a function named `rotateMatrix()` that rectangular array of numbers (a matrix) and returns the matrix rotated 90 degrees to the right.
 
+1. Do you understand the problem?
+  - Yes: Talk it out with the interviewer, making sure you understand what is expected
+  - No: clarify the problem, ask "what ifs", clarify inputs and outputs, ask for more samples, think of hypotheticals
+  - Still no: Try something a bit easier, how do you break this problem down ("I have no idea where to start, but I know how to write a loop - so I am going to start there")
+2. Write psuedocode
+  - Clarify that you are going in the right direction
+3. Write sandwich code
+4. Wite code
+5. Test - manually test, automate those tests
+6. Burn it down, refactor, do it again, test, burn it down, refactor
+
  */
 
-// function rotateMatrix(matrix) {
-//   let newArray = [];
-//   let result = [];
-//   for (let i = 0; i < matrix[0].length; i++) {
-//     newArray = [];
-//     for (let j = 0; j < matrix.length; j++) {
-//       if (matrix[j]) {
-//         newArray.unshift(matrix[j][i]);
-//       }
-//     }
-//     result.push(newArray);
-//   }
-//   return result;
-// }
-
+// brute force
 function rotateMatrix(matrix) {
-  return matrix[0].map((col, i) => {
-    return matrix.map((row, j) => {
-      let neg = matrix.length - (j + 1);
-      return matrix[neg][i];
-    });
-  });
+  let newArray = [];
+  let result = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    newArray = [];
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[j]) {
+        newArray.unshift(matrix[j][i]);
+      }
+    }
+    result.push(newArray);
+  }
+  return result;
 }
+
+// function rotateMatrix(matrix) {
+//   return matrix[0].map((col, i) => {
+//     return matrix.map((row, j) => {
+//       let neg = matrix.length - (j + 1);
+//       return matrix[neg][i];
+//     });
+//   });
+// }
 
 const matrix1 = [
   [44, 23],
